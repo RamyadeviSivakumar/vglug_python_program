f38d8cad6840856e4173a3d90515191c3d61aea2
RamyadeviSivakumar <sivakumarvengadesan@gmail.com>
2024-10-19 21:10:39 +0530

    inhitance program added

---
+++ inheritance.py
@@ -0,0 +1,14 @@
+class grandfather():
+    def method1(self):
+        print("this is the grandpa class")
+class father():
+    def method2(self):
+        print("this is father class")
+class son(grandfather,father):
+    def method3(self):
+        print("this is odn class")
+
+object=son()
+object.method1() #accessing grandfather class by son's object
+object.method2() #accessing father class by son's object
+object.method3()# accessing son class
